3aba9935f83fad0b84852de29f1a92649f1d2c3b
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-06-03 15:45:11 +0100

    Now output works
    Also was a problem with input file - the first entry had 2 spaces
    not one and it was getting all in the way of the first one working
    properly.

--- table_manip.pl
+++ table_manip.pl
@@ -6,14 +6,16 @@ use strict;
 die "usage: table_manip.pl datafile" unless (@ARGV == 1);
 
 my $filename = shift;
+my $out_file = $filename . "output.txt";
 
-open(my $table_file, '<', $filename);
+open(my $table_file, '<', $filename) or die "can't open file $filename, $!";
+open(my $output_file, '>', $out_file) or die "can't open file $filename, $!";
 
 while (my $line = <$table_file>) {
     # use a regex to find the gene name and length
-    $line =~ m/^(\w+)\s[-\+]\s\d+-\d+\s(\d+)/;
+    $line =~ m/(\w+)\s[-\+]\s\d+-\d+\s(\d+)/;
 
     # print to output file to be used in R
-    print "$1 $2\n";
+    print $output_file "$1 $2\n";
 }
 
